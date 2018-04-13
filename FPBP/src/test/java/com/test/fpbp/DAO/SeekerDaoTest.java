c1d8e90b437ba69ff2ee66d1d8cfe85933617305
coolp <coolp@LAPTOP-THUIN0PM>
2018-04-13 04:23:18 -0400

    Validation check

--- FPBP/src/test/java/com/test/fpbp/DAO/SeekerDaoTest.java
+++ FPBP/src/test/java/com/test/fpbp/DAO/SeekerDaoTest.java
@@ -9,24 +9,39 @@ import javax.sql.DataSource;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.fpbp.DAO.DonorDAO;
-import com.fpbp.DAO.DonorDAOImpl;
-import com.fpbp.model.Donor;
+import com.fpbp.DAO.SeekerDAO;
+import com.fpbp.DAO.SeekerDAOImpl;
+import com.fpbp.model.Seeker;
 import com.test.fpbp.connection.ConnectionTest;
 
 public class SeekerDaoTest {
 
 	
 	 @Test
-	    public void testFetchProviders() {
+	    public void testFetchSeekers() {
 	    	
 	    	ConnectionTest ct = new ConnectionTest();
 	    	DataSource ds=ct.getSource();
 	        JdbcTemplate jt = new JdbcTemplate(ds);
-	        DonorDAO d=new DonorDAOImpl(ds,jt); 
-			List<Donor> msg =  d.fetchProviders("Food");
+	        SeekerDAO d=new SeekerDAOImpl(ds,jt); 
+			List<Seeker> msg =  d.fetchSeekers("Food");
 			assertEquals(msg.size(),2);
 			
 	        
 	    }
+	 
+	 @Test
+	 public void testAddSeeker() {
+		 
+		 ConnectionTest ct = new ConnectionTest();
+	    	DataSource ds=ct.getSource();
+	        JdbcTemplate jt = new JdbcTemplate(ds);
+	        
+	        SeekerDAO d=new SeekerDAOImpl(ds,jt); 
+	        Seeker s = new Seeker("PriTest","Charlotte","980980","uncc.com","uncc@uncc.edu","Food",50,"Food");
+			boolean msg =  d.addSeeker(s);
+			assertEquals(msg,true);
+			jt.update("delete from fpbpTest.helpSeeker where org_name='PriTest'");
+		 
+	 }
 }
