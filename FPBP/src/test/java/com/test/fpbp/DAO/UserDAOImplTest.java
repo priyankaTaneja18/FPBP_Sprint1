6c29d6866deac9819370372ee70a08d703ff5e98
coolp <coolp@LAPTOP-THUIN0PM>
2018-04-26 19:48:18 -0400

    UserDao Sprint 3

--- FPBP/src/test/java/com/test/fpbp/DAO/UserDAOImplTest.java
+++ FPBP/src/test/java/com/test/fpbp/DAO/UserDAOImplTest.java
@@ -26,13 +26,15 @@ public class UserDAOImplTest   {
     	DataSource ds=ct.getSource();
         JdbcTemplate jt = new JdbcTemplate(ds);
         UserDAO d=new UserDAOImpl(ds,jt); 
+        jt.update("delete from fpbpTest.fpbpUsers");
+        jt.update("Insert into fpbpTest.fpbpUsers(email,password) values('user@test.com','test1234')");
 		try {
-			msg = d.validateUser("use22r@test.com", "test1234");
+			msg = d.validateUser("user@test.com", "test1234");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
         assertEquals(msg,"Login Successful");
-        
+        jt.update("delete from fpbpTest.fpbpUsers");
         //assertEquals(categories.get(0),"Clothing");
         
     }
@@ -45,20 +47,47 @@ public class UserDAOImplTest   {
     	DataSource ds=ct.getSource();
         JdbcTemplate jt = new JdbcTemplate(ds);
         UserDAO d=new UserDAOImpl(ds,jt); 
+        jt.update("delete from fpbpTest.fpbpUsers");
+       
 		try {
-			msg = d.registerUser("user@test.com", "test1234");
-			msg2 = d.registerUser("tryyhs@test.com", "test1234");
+			
+			msg2 = d.registerUser("try@test.com", "test1234");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        assertEquals(msg,"Email id already exists");
+     
         assertEquals(msg2,"Account created successfully");
         //assertEquals(categories.get(0),"Clothing");
         String sql = "SELECT count(email) FROM fpbpUsers where email=? ";
 
 		//int result = getJdbcTemplate().queryForObject(sql, new Object[] {email});
-		int result = jt.queryForObject(sql, Integer.class,new Object[] {"abc@test.com"});
+		int result = jt.queryForObject(sql, Integer.class,new Object[] {"try@test.com"});
 		assertEquals(result, 1);
+		jt.update("delete from fpbpTest.fpbpUsers");
+        
+    }
+    
+    
+    @Test
+    public void RegisterUserTest_InValid() {
+    	String msg= null;
+    	String msg2 =null;
+    	ConnectionTest ct = new ConnectionTest();
+    	DataSource ds=ct.getSource();
+        JdbcTemplate jt = new JdbcTemplate(ds);
+        UserDAO d=new UserDAOImpl(ds,jt); 
+        jt.update("delete from fpbpTest.fpbpUsers");
+        jt.update("Insert into fpbpTest.fpbpUsers(email,password) values('user@test.com','test1234')");
+		
+		try {
+			msg = d.registerUser("user@test.com", "test1234");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        assertEquals(msg,"Email id already exists");
+       
+		jt.update("delete from fpbpTest.fpbpUsers");
         
     }
 }
